9624f03141b613c0cad92c773ed0f71989019528
pyphy <pyphy@usepm.com>
2021-08-12 05:18:57 +0000

    ルーターの設定

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,13 +9,9 @@ import { MessageService } from '../message.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero?: Hero;
   heroes: Hero[] = [];
 
-  constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
-  ) {}
+  constructor(private heroService: HeroService) {}
 
   /**
    * 一番最初に自動実行される
@@ -29,14 +25,5 @@ export class HeroesComponent implements OnInit {
    */
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
-  }
-
-  /**
-   * クリックしたとき
-   * @param hero
-   */
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
